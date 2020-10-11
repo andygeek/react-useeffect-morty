6925b95b0cb16f3884029aafa4456dba8896dbb8
AndyGeek <andy.eulogio@gmail.com>
2020-10-10 19:49:54 -0500

    UseEffect already done in project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const App = () => {
+	const [resourceType, setResourceType] = useState('character');
+	const [items, setItems] = useState([])
+	useEffect(() => {
+		fetch(`https://rickandmortyapi.com/api/${resourceType}`)
+			.then((response) => response.json())
+			.then((json) => setItems(json.results));
+	}, [resourceType]);
 	return (
-		<div className="App">
-			<h1>Hola mundo</h1>
-		</div>
+		<>
+			<div>
+				<button onClick={() => setResourceType('character')}>Character</button>
+				<button onClick={() => setResourceType('location')}>Location</button>
+				<button onClick={() => setResourceType('episode')}>Episode</button>
+			</div>
+			<h1>{resourceType}</h1>
+			{
+				items.map(item => (
+					<pre key={item.id}>{JSON.stringify(item)}</pre>
+				))
+			}
+		</>
 	);
 };
